c63502cc1788cb1990245fc3c32cae80eccb9eac
Damien Tanner <damien@new-bamboo.co.uk>
2008-06-06 19:15:19 +0100

    simple_db.rb crud working

--- lib/simple_db.rb
+++ lib/simple_db.rb
@@ -7,16 +7,68 @@ class SimpleDB
     
     def self.connection; @@connection; end
     
-    def self.set_domain(domain)
-      @@domain = domain
-    end
-    
-    def domain
-      @@connection.domain(@@domain)
+    class << self
+      attr_accessor :domain_name
     end
     
     def self.domain
-      @@connection.domain(@@domain)
+      @@connection.domain(self.domain_name)
+    end
+    
+    def self.set_domain(d)
+      self.domain_name = d
+    end
+    
+    attr_accessor :key, :attributes
+
+    def initialize(key=nil, multimap_or_hash=nil)
+      self.key = (key || UUID.new)
+      self.attributes = multimap_or_hash.nil? ? Amazon::SDB::Multimap.new : (multimap_or_hash.kind_of?(Hash) ? Amazon::SDB::Multimap.new(multimap_or_hash) : multimap_or_hash)
+    end
+
+    def self.create(values)
+      self.key = UUID.new
+      self.attributes = Amazon::SDB::Multimap.new(*values)
+      self.new(key, attributes)
+    end
+
+    def self.create!(values)
+      video = self.create(values)
+      video.save
+      video
+    end
+
+    def [](key)
+      self.attributes[key]
+    end
+
+    def []=(key, value)
+      self.attributes[key] = value
+    end
+
+    def save
+      self.class.domain.put_attributes(self.key, self.attributes, :replace => :all)
+    end
+    
+    def destroy!
+      self.class.domain.delete_attributes(self.key)
+    end
+    
+    def reload!
+      item = self.class.domain.get_attributes(@key)
+      self.attributes = item.attributes
+    end
+
+    def self.find(key)
+      self.new(key, self.domain.get_attributes(key).attributes)
+    end
+
+    def self.query(query_options='')
+      result = []
+      self.domain.query(query_options).each do |i|
+        result << self.new(i.key, i.attributes)
+      end
+      return result
     end
   end
 end
