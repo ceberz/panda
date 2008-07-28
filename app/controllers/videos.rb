35b073cd87c973e8e6808e98169131fd6e214e31
Damien Tanner <damien@new-bamboo.co.uk>
2008-07-28 22:40:12 +0100

    Use minimal layout for exceptions

--- app/controllers/videos.rb
+++ app/controllers/videos.rb
@@ -155,7 +155,7 @@ private
         "<textarea>" + {:error => msg}.to_json + "</textarea>"
       else
         @exception = msg
-        render(:template => "/exceptions/video_exception") # TODO: Why is :action setting 404 instead of 500?!?!
+        render(:template => "/exceptions/video_exception", :layout => false) # TODO: Why is :action setting 404 instead of 500?!?!
       end
     when :xml
       {:error => msg}.to_simple_xml
