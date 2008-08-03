c2e14b5aa3752e7595d8e5422c32aec514621648
Damien Tanner <damien@new-bamboo.co.uk>
2008-08-03 13:51:32 +0100

    use config defaults

--- lib/panda.rb
+++ lib/panda.rb
@@ -17,11 +17,7 @@ module Panda
   end
 
   class Config
-
     class << self
-
-      # ==== Returns
-      # Hash:: The defaults for the config.
       def defaults
         @defaults ||= {
           :account_name           => "My Panda Account",
@@ -29,45 +25,20 @@ module Panda
           :upload_redirect_url    => "http://127.0.0.1:3000/videos/$1/done",
           :state_update_url       => "http://127.0.0.1:3000/videos/$1/status",
           :videos_domain          => nil,
-          :storage                => :filesystem # or :s3 TODO: implement
+          :storage                => :filesystem, # or :s3 TODO: implement
+          :thumbnail_height_constrain => 126
         }
       end
-
-      # Yields the configuration.
-      #
-      # ==== Block parameters
-      # c<Hash>:: The configuration parameters.
-      #
-      # ==== Examples
-      #   Merb::Config.use do |config|
-      #     config[:exception_details] = false
-      #   end
+      
       def use
         @configuration ||= {}
         yield @configuration
       end
 
-      # ==== Parameters
-      # key<Object>:: The key to check.
-      #
-      # ==== Returns
-      # Boolean:: True if the key exists in the config.
-      def key?(key)
-        @configuration.key?(key)
-      end
-
-      # ==== Parameters
-      # key<Object>:: The key to retrieve the parameter for.
-      #
-      # ==== Returns
-      # Object:: The value of the configuration parameter.
       def [](key)
-        (@configuration||={})[key]
+        @configuration[key] || defaults[key]
       end
-
-      # ==== Parameters
-      # key<Object>:: The key to set the parameter for.
-      # val<Object>:: The value of the parameter.
+      
       def []=(key,val)
         @configuration[key] = val
       end
