71088715923776e0f2c0b123f58cbf887d8f5cb7
ceberz <zortnac@gmail.com>
2008-09-25 10:42:34 -0700

    fixing a bug not caught in specs

--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -5,7 +5,7 @@ class Dashboard < Application
     @recent_encodings = Video.recent_encodings
     @queued_encodings = Video.queued_encodings
     
-    encode_queue = EncodeQueue.new
+    job_queue = EncodeQueue.new
     @num_jobs_in_queue = job_queue.num_jobs
     @queued_in_qb = Video.query("['status' = 'queued']")
     @num_running_threads = EncoderSingleton.job_count
